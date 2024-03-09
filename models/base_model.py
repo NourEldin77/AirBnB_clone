1079214a1dc7db1559127487b4b9cef5b8148ca0
NourEldin77 <noureldean.2003@gmail.com>
2024-03-09 04:31:30 +0200

    feat: base_model arg parse

--- models/base_model.py
+++ models/base_model.py
@@ -13,7 +13,7 @@ class BaseModel:
         save: update date/time
         __str__: string repr for object
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
     constructor function for BaseModel class
     :param:
@@ -23,10 +23,22 @@ class BaseModel:
     :return:
         void
     """
-        self.id = str(uuid.uuid4())
-
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+        if (kwargs):
+            for key in kwargs.keys():
+                if key == "created_at":
+                    self.created_at = datetime.datetime.strptime(
+                            kwargs['created_at'],
+                            "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.updated_at = datetime.datetime.strptime(
+                            kwargs['updated_at'],
+                            "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = kwargs[key]
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
 
     def save(self):
         """ update time with current datetime"""
